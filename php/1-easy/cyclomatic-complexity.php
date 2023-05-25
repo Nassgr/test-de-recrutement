2528aa4103a88bc7da34f2aeb35feb8daf0eb02d
Nassgr <35959360+Nassgr@users.noreply.github.com>
2023-05-25 14:20:17 +0200

    Update cyclomatic-complexity.php

--- php/1-easy/cyclomatic-complexity.php
+++ php/1-easy/cyclomatic-complexity.php
@@ -1,59 +1,27 @@
 <?php
 
 function convertSize($bytes, $precision = 2) {
-  $kilobytes = $bytes / 1024;
+  $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB', 'HB'];
+  $index = 0;
+  $value = $bytes;
 
-  if ($kilobytes < 1024) {
-    return round($bytes, $precision) . ' KB';
+  while ($value >= 1024) {
+    if ($index >= count($units) - 1) {
+      break;
+    }
+
+    $value /= 1024;
+    $index++;
   }
 
-  $megabytes = $kilobytes / 1024;
-
-  if ($megabytes < 1024) {
-    return round($megabytes, $precision) . ' MB';
-  }
-
-  $gigabytes = $megabytes / 1024;
-
-  if ($gigabytes < 1024) {
-    return round($gigabytes, $precision) . ' GB';
-  }
-
-  $terabytes = $gigabytes / 1024;
-
-  if ($terabytes < 1024) {
-    return round($terabytes, $precision) . ' TB';
-  }
-
-  $petabytes = $terabytes / 1024;
-
-  if ($petabytes < 1024) {
-    return round($petabytes, $precision) . ' TB';
-  }
-
-  $exabytes = $petabytes / 1024;
-
-  if ($exabytes < 1024) {
-    return round($exabytes, $precision) . ' EB';
-  }
-
-  $zettabytes = $exabytes / 1024;
-
-  if ($zettabytes < 1024) {
-    return round($zettabytes, $precision) . ' ZB';
-  }
-
-  $yottabytes = $zettabytes / 1024;
-
-  if ($yottabytes < 1024) {
-    return round($yottabytes, $precision) . ' ZB';
-  }
-
-  $hellabyte = $yottabytes / 1024;
-
-  if ($hellabyte < 1024) {
-    return round($hellabyte, $precision) . ' HB';
-  }
-  
-  return $bytes . ' B';
+  return round($value, $precision) . ' ' . $units[$index];
 }
+
+/* Les unités sont constantes donc on peut les rajouter en haut elle ne changerons jamais.
+
+Ensuite une boucle où nous vérifions d'abord si la condition de sortie est satisfaite ($index >= count($units) - 1). 
+Si tel est le cas, nous utilisons break pour sortir de la boucle. 
+Sinon, nous continuons à diviser $value par 1024 et à incrémenter $index.
+
+On retourne l'arrondi grace à la fn php round() avec l'unité trouvé dans la boucle en concaténation.
+*/

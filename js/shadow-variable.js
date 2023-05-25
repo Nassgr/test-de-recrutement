c2982d3deeb8c454cbd393db78e7783f198e209c
Nassgr <35959360+Nassgr@users.noreply.github.com>
2023-05-25 14:00:55 +0200

    Update shadow-variable.js

--- js/shadow-variable.js
+++ js/shadow-variable.js
@@ -1,17 +1,27 @@
-const articleList = []; // In a real app this list would be full of articles.
-var kudos = 5;
+const articleList = []; // Dans une véritable application, cette liste serait remplie d'articles.
+const maxKudos = 5;
 
 function calculateTotalKudos(articles) {
-  var kudos = 0;
-  
+  let totalKudos = 0;
+
   for (let article of articles) {
-    kudos += article.kudos;
+    totalKudos += article.kudos;
   }
-  
-  return kudos;
+
+  return totalKudos;
 }
 
-document.write(`
-  <p>Maximum kudos you can give to an article: ${kudos}</p>
-  <p>Total Kudos already given across all articles: ${calculateTotalKudos(articleList)}</p>
-`);
+const output = `
+  <p>Nombre maximum de kudos que vous pouvez donner à un article : ${maxKudos}</p>
+  <p>Total des kudos déjà attribués à tous les articles : ${calculateTotalKudos(articleList)}</p>
+`;
+
+document.write(output);
+
+/*
+
+Dans ce fichier JS, la refactorisation est surtout centré sur le nommage des variables qui pour moi peut porter à confusion :
+kudos est affiché deux fois on peut le remplacer par une const maxKudos et une variable let totalKudos par exemple.
+Enfin j'ai mis l'output dans une const à la fin car plus lisible.
+
+*/

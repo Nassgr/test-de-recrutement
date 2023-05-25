8794afa4301d0672fdd0adfd249e129d0a38a392
Nassgr <35959360+Nassgr@users.noreply.github.com>
2023-05-25 18:03:01 +0200

    Update Rover.php

--- php/2-medium/Rover.php
+++ php/2-medium/Rover.php
@@ -19,55 +19,49 @@ class Rover
 
     public function receive(string $commandsSequence): void
     {
-        $commandsSequenceLenght = strlen($commandsSequence);
-        for ($i = 0; $i < $commandsSequenceLenght; ++$i) {
-            $command = substr($commandsSequence, $i, 1);
+        $commandsSequenceLength = strlen($commandsSequence);
+
+        for ($i = 0; $i < $commandsSequenceLength; ++$i) {
+            $command = $commandsSequence[$i];
+
             if ($command === "l" || $command === "r") {
-                // Rotate Rover
-                if ($this->direction === "N") {
-                    if ($command === "r") {
-                        $this->direction = "E";
-                    } else {
-                        $this->direction = "W";
-                    }
-                } else if ($this->direction === "S") {
-                    if ($command === "r") {
-                        $this->direction = "W";
-                    } else {
-                        $this->direction = "E";
-                    }
-                } else if ($this->direction === "W") {
-                    if ($command === "r") {
-                        $this->direction = "N";
-                    } else {
-                        $this->direction = "S";
-                    }
-                } else {
-                    if ($command === "r") {
-                        $this->direction = "S";
-                    } else {
-                        $this->direction = "N";
-                    }
-                }
+                $this->rotateRover($command);
             } else {
-                // Displace Rover
-                $displacement1 = -1;
-
-                if ($command === "f") {
-                    $displacement1 = 1;
-                }
-                $displacement = $displacement1;
-
-                if ($this->direction === "N") {
-                    $this->y += $displacement;
-                } else if ($this->direction === "S") {
-                    $this->y -= $displacement;
-                } else if ($this->direction === "W") {
-                    $this->x -= $displacement;
-                } else {
-                    $this->x += $displacement;
-                }
+                $this->displaceRover($command);
             }
         }
     }
+
+    private function rotateRover(string $command): void
+    {
+        $rotations = [
+            "N" => ["r" => "E", "l" => "W"],
+            "E" => ["r" => "S", "l" => "N"],
+            "S" => ["r" => "W", "l" => "E"],
+            "W" => ["r" => "N", "l" => "S"],
+        ];
+
+        $this->direction = $rotations[$this->direction][$command];
+    }
+
+    private function displaceRover(string $command): void
+    {
+        $displacement = ($command === "f") ? 1 : -1;
+
+        if ($this->direction === "N") {
+            $this->y += $displacement;
+        } else if ($this->direction === "S") {
+            $this->y -= $displacement;
+        } else if ($this->direction === "W") {
+            $this->x -= $displacement;
+        } else {
+            $this->x += $displacement;
+        }
+    }
 }
+
+/* Explications : 
+Dans cette Class, je propose un split de la function receive en 3 function distincte plus parlante 
+(receive donc puis rotateRover et displaceRover).
+La function rotateRover intégre la logique de rotation du rover dans un tableau associatif $rotations.
+La function displaceRover intégre la logique du déplacement du rover en testant les conditions nord sud ouest est
